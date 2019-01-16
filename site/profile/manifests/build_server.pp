8204f68a779b102bd7769b50d7526de0697ae8d5
Brian Wint <BWint@infragistics.com>
2019-01-16 13:24:19 -0500

    try and fix path

--- site/profile/manifests/build_server.pp
+++ site/profile/manifests/build_server.pp
@@ -2,14 +2,14 @@
 class profile::build_server{
 
 file { 'internal-tools-suite-installer':
-path => 'C/temp/Internal Tools Suite.msi',
+path => 'C:/temp/Internal Tools Suite.msi',
 ensure => present,
 source => 'http://builds.infragistics.com/products/Tools/Internal Tools Suite.msi',
 }
 
   package { "infragistics internal tools suite":
   ensure => installed,
-  source => 'C/temp/Internal Tools Suite.msi',
+  source => 'C:/temp/Internal Tools Suite.msi',
   install_options => [ '/qn' ],
   uninstall_options => [ '/x', '/qn'],
   require => File['internal-tools-suite-installer'],
