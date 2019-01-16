acbcdfc1ad5ab04877abf5b4d734e87405df15d7
Brian Wint <BWint@infragistics.com>
2019-01-16 13:45:04 -0500

    update file resource for msi file

--- site/profile/manifests/build_server.pp
+++ site/profile/manifests/build_server.pp
@@ -1,15 +1,14 @@
 # The base level content for a build server
 class profile::build_server{
 
-file { 'internal-tools-suite-installer':
-path => 'C:/temp/Internal Tools Suite.msi',
+file { 'c:/temp/Internal Tools Suite.msi':
 ensure => present,
 source => 'http://builds.infragistics.com/products/Tools/Internal%20Tools%20Suite.msi',
 }
 
   package { "infragistics internal tools suite":
   ensure => installed,
-  source => 'C:/temp/Internal Tools Suite.msi',
+  source => 'c:/temp/Internal Tools Suite.msi',
   install_options => [ '/qn' ],
   uninstall_options => [ '/x', '/qn'],
   require => File['internal-tools-suite-installer'],
