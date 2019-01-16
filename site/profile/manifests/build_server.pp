dfaaa442c314057fddc52c70517406b91a3c3763
Brian Wint <BWint@infragistics.com>
2019-01-16 13:19:47 -0500

    add build server base profile that a us_build_server can utilize

---
+++ site/profile/manifests/build_server.pp
@@ -0,0 +1,18 @@
+# The base level content for a build server
+class profile::build_server{
+
+file { 'internal-tools-suite-installer':
+path => 'C/temp/Internal Tools Suite.msi',
+ensure => present,
+source => 'http://builds.infragistics.com/products/Tools/Internal Tools Suite.msi',
+}
+
+  package { "infragistics internal tools suite":
+  ensure => installed,
+  source => 'C/temp/Internal Tools Suite.msi',
+  install_options => [ '/qn' ],
+  uninstall_options => [ '/x', '/qn'],
+  require => File['internal-tools-suite-installer'],
+}
+
+}
--- site/profile/manifests/us_build_server.pp
+++ site/profile/manifests/us_build_server.pp
@@ -1,5 +1,7 @@
 class profile::us_build_server{
   
+  include profile:build_server
+
   class { 'timezone_win':
     timezone => 'Eastern Standard Time',
   }
