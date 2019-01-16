e60b0f23be373737ea9e3f066cb0e297e4502fc2
Brian Wint <BWint@infragistics.com>
2019-01-16 15:27:34 -0500

    add checksum back in and log file options

--- site/profile/manifests/build_server.pp
+++ site/profile/manifests/build_server.pp
@@ -8,14 +8,15 @@ ensure => directory,
 file { 'c:/temp/Internal Tools Suite.msi':
 ensure => present,
 source => 'http://builds.infragistics.com/products/Tools/Internal%20Tools%20Suite.msi',
+checksum => md5,
 require => File['c:/temp'],
 }
 
   package { "infragistics internal tools suite":
   ensure => installed,
   source => 'c:/temp/Internal Tools Suite.msi',
-  install_options => [ '/qn' ],
-  uninstall_options => [ '/x', '/qn' ],
+  install_options => [ '/lvx* c:/temp/install_log.txt' ],
+  uninstall_options => [ '/lvx* c:/temp/uninstall_log.txt'],
   require => File['c:/temp/Internal Tools Suite.msi'],
 }
 
