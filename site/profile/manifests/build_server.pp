4c09799568153a407fc8e3a0b29e51f66d438fdf
Brian Wint <BWint@infragistics.com>
2019-01-16 14:00:49 -0500

    adding a fiel resource to define the temp folder.

--- site/profile/manifests/build_server.pp
+++ site/profile/manifests/build_server.pp
@@ -1,9 +1,14 @@
 # The base level content for a build server
 class profile::build_server{
 
+file { 'c:/temp':
+ensure => present,
+}
+
 file { 'c:/temp/Internal Tools Suite.msi':
 ensure => present,
 source => 'http://builds.infragistics.com/products/Tools/Internal%20Tools%20Suite.msi',
+require => File['c:/temp'],
 }
 
   package { "infragistics internal tools suite":
