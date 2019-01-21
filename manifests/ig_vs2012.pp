771b45dda84fc28a629dbe069bf2f63ce7f8a1ad
Brian Wint <BWint@infragistics.com>
2019-01-21 15:34:30 -0500

    updating require reference

--- manifests/ig_vs2012.pp
+++ manifests/ig_vs2012.pp
@@ -2,12 +2,12 @@ class ig_vs2012 {
 
 file { 'C:\temp\VS2012.5.iso':
   ensure => file,
-  source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
+  source => '\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
 }
 
 mount_iso { 'C:\temp\VS2012.5.iso':
   drive_letter => 'H',
-  require => File['VS2012.5.iso'],
+  require => File['C:\temp\VS2012.5.iso'],
 }
 
 }
