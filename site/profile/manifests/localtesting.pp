0802a18e9b7f1163ff10769cfdb6166254c5c202
Brian Wint <BWint@infragistics.com>
2019-01-22 14:50:35 -0500

    small file testing worked. just would take take to copy over a large file

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,16 +1,10 @@
 class profile::localtesting{
 
-file { 'Internal Tools Suite.wixpdb':
+file { 'VS2012.5.iso':
   ensure => present,
-  path => 'C:\temp\Internal Tools Suite.wixpdb',
-  source => '\\\\infragistics.local\igfiles\Localbuilds\Internal Tools Suite\Internal Tools Suite_1.0.0.1181\Internal Tools Suite.wixpdb',
+  path => 'C:\temp\VS2012.5.iso',
+  source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
 }
-
-# file { 'VS2012.5.iso':
-#   ensure => present,
-#   path => 'C:\temp\VS2012.5.iso',
-#   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
-# }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
 #   ensure => present,
