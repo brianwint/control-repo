59bb5476fdef6419e773e62fca73cec487380fd6
Brian Wint <BWint@infragistics.com>
2019-01-22 14:31:38 -0500

    try a very small file just to test unc path

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,10 +1,16 @@
 class profile::localtesting{
 
-file { 'VS2012.5.iso':
+file { 'Internal Tools Suite.wixpdb':
   ensure => present,
-  path => 'C:\temp\VS2012.5.iso',
-  source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+  path => 'C:\temp\Internal Tools Suite.wixpdb',
+  source => '\\\\infragistics.local\igfiles\Localbuilds\Internal Tools Suite\Internal Tools Suite_1.0.0.1181\Internal Tools Suite.wixpdb',
 }
+
+# file { 'VS2012.5.iso':
+#   ensure => present,
+#   path => 'C:\temp\VS2012.5.iso',
+#   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+# }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
 #   ensure => present,
