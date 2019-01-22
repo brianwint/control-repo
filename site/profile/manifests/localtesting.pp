ccb5ad2fec7a49ee418eb3b7b07adcafd179113c
Brian Wint <BWint@infragistics.com>
2019-01-22 12:01:27 -0500

    update path syntax

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -2,8 +2,8 @@ class profile::localtesting{
 
 file { 'VS2012.5.iso':
   ensure => present,
-  path => 'C:\\temp\\VS2012.5.iso',
-  source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
+  path => 'C:/temp/VS2012.5.iso',
+  source => '//infragistics.local/igfiles/Software/Microsoft/Development/Visual Studio.NET 2012/Update 5/VS2012.5.iso',
 }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
