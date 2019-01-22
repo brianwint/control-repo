02814bd4d1667fe61a4b3687350de5a7c6aca9ad
Brian Wint <BWint@infragistics.com>
2019-01-22 16:42:42 -0500

    test mount iso

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,8 +1,12 @@
 class profile::localtesting{
 
-file { 'C:\temp\VS2012.5.iso':
-  ensure => present,
-  source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+# file { 'C:\temp\VS2012.5.iso':
+#   ensure => present,
+#   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+# }
+
+mount_iso { 'C:\temp\VS2012.5.iso':
+  drive_letter => 'H',
 }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
