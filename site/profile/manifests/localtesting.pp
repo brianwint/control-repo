dcd370a1f56cbb76f7a6e8f84375927917b694a5
Brian Wint <BWint@infragistics.com>
2019-01-22 11:33:49 -0500

    just look to see if the file is present

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,7 +1,7 @@
 class profile::localtesting{
 
 file { 'VS2012.5.iso':
-  ensure => file,
+  ensure => present,
   path => 'C:/temp/VS2012.5.iso',
   source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
 }
