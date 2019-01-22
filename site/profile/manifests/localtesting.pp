326588a1712eec33b5f54ae9fd5263ca652d95c1
Brian Wint <BWint@infragistics.com>
2019-01-22 11:36:56 -0500

    update path

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -2,11 +2,11 @@ class profile::localtesting{
 
 file { 'VS2012.5.iso':
   ensure => present,
-  path => 'C:/temp/VS2012.5.iso',
+  path => 'C:\\temp\\VS2012.5.iso',
   source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
 }
 
-mount_iso { 'C:/temp/VS2012.5.iso':
+mount_iso { 'C:\temp\VS2012.5.iso':
   drive_letter => 'H',
   require => File['VS2012.5.iso'],
 }
