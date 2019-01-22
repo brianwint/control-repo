6e390352f1c8bf2cccc4c9303860ea35f7bb8f18
Brian Wint <BWint@infragistics.com>
2019-01-22 11:48:35 -0500

    comment out the mounting piece. just make sure the fiel piece works fine on its own.

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -6,9 +6,9 @@ file { 'VS2012.5.iso':
   source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
 }
 
-windows_isos { 'C:\temp\VS2012.5.iso':
-  ensure => present,
-  isopath => 'C:\\temp\\VS2012.5.iso',
-}
+# windows_isos { 'C:\temp\VS2012.5.iso':
+#   ensure => present,
+#   isopath => 'C:\\temp\\VS2012.5.iso',
+# }
 
 }
