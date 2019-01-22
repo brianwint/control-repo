af57a89ffe53b9a09788e300813773d5b55a4e3b
Brian Wint <BWint@infragistics.com>
2019-01-22 11:30:57 -0500

    try modifying path variables

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,13 +1,14 @@
 class profile::localtesting{
 
-file { 'C:\temp\VS2012.5.iso':
+file { 'VS2012.5.iso':
   ensure => file,
-  source => '\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+  path => 'C:/temp/VS2012.5.iso',
+  source => '\\\\infragistics.local\\igfiles\\Software\\Microsoft\\Development\\Visual Studio.NET 2012\\Update 5\\VS2012.5.iso',
 }
 
-mount_iso { 'C:\temp\VS2012.5.iso':
+mount_iso { 'C:/temp/VS2012.5.iso':
   drive_letter => 'H',
-  require => File['C:\temp\VS2012.5.iso'],
+  require => File['VS2012.5.iso'],
 }
 
 }
