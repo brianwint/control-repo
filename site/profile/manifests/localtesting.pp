a3fe2766af2766b252afad514556d6ec4ecd6ba3
Brian Wint <BWint@infragistics.com>
2019-01-22 15:13:07 -0500

    try disabling checksum for faster file compare?

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -1,9 +1,9 @@
 class profile::localtesting{
 
-file { 'VS2012.5.iso':
+file { 'C:\temp\VS2012.5.iso':
   ensure => present,
-  path => 'C:\temp\VS2012.5.iso',
   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
+  checksum => none,
 }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
