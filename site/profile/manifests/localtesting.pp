63e0a9c14e4e273063c376b057b3abb6b27c6c87
Brian Wint <BWint@infragistics.com>
2019-01-23 15:19:33 -0500

    try installing VS2010 from a network mapped drive

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -5,9 +5,15 @@ class profile::localtesting{
 #   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
 # }
 
-mount_iso { 'C:\temp\VS2012.5.iso':
-  drive_letter => 'H',
-}
+package { 'Y:\VS2010SP1dvd1\Setup.exe':
+  name => "Microsoft Visual Studio 2010 Premium - ENU",
+  ensure => installed,
+  install_options => [ '/log*', 'c:/temp/vs2010_install_log.txt' ],
+  }
+
+# mount_iso { 'C:\temp\VS2012.5.iso':
+#   drive_letter => 'H',
+# }
 
 # windows_isos { 'C:\temp\VS2012.5.iso':
 #   ensure => present,
