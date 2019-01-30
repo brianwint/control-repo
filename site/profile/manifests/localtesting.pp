381c7be94988349c83e9db0631b32d231d028ef8
Brian Wint <BWint@infragistics.com>
2019-01-30 13:56:51 -0500

    update to install just VS2017 with basic .NET managed feature and coded UI in a passive mode.

--- site/profile/manifests/localtesting.pp
+++ site/profile/manifests/localtesting.pp
@@ -5,19 +5,29 @@ class profile::localtesting{
 #   source => '\\\\infragistics.local\igfiles\Software\Microsoft\Development\Visual Studio.NET 2012\Update 5\VS2012.5.iso',
 # }
 
-package { 'vs2010base':
-  name => 'Microsoft Visual Studio 2010 Premium - ENU',
-  ensure => installed,
-  source => 'Y:/VS2010Core/Setup/setup.exe',
-  install_options => [ '/q', '/UnattendFile', 'Y:\VS2010Core\Setup\vs2010options.ini' ],
-  }
+# package { 'vs2010base':
+#   name => 'Microsoft Visual Studio 2010 Premium - ENU',
+#   ensure => installed,
+#   source => 'Y:/VS2010Core/Setup/setup.exe',
+#   install_options => [ '/q', '/UnattendFile', 'Y:\VS2010Core\Setup\vs2010options.ini' ],
+#   }
 
-package { 'vs2010sp1':
-  name => 'Microsoft Visual Studio 2010 Premium SP1 - ENU',
+# package { 'vs2010sp1':
+#   name => 'Microsoft Visual Studio 2010 Premium SP1 - ENU',
+#   ensure => installed,
+#   source => 'Y:/VS2010SP1dvd1/Setup.exe',
+#   install_options => [ '/q', '/log', 'c:/temp/vs2010_sp1_install_log.txt' ],
+#   require => Package['vs2010base'],
+#   }
+
+package { 'vs2017Enterprise':
+  name => 'Microsoft Visual Studio 2017 Enterprise - ENU',
   ensure => installed,
-  source => 'Y:/VS2010SP1dvd1/Setup.exe',
-  install_options => [ '/q', '/log', 'c:/temp/vs2010_sp1_install_log.txt' ],
-  require => Package['vs2010base'],
+  source => 'Y:/vs_enterprise.exe',
+  install_options => [ '--passive',
+  '-add', 'Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended',
+  '-add', 'Microsoft.VisualStudio.Component.TestTools.CodedUITest',
+  '--norestart' ],
   }
 
 # mount_iso { 'C:\temp\VS2012.5.iso':
